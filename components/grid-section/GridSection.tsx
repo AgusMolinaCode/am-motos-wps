abf5e437d27873fcd0560ca2585371c9c0ac5477
Agustin <am.motos@hotmail.com>
2025-02-12 01:15:33 -0300

    Refine GridSection and WobbleCard components with enhanced hover effects and responsive design

--- components/grid-section/GridSection.tsx
+++ components/grid-section/GridSection.tsx
@@ -9,7 +9,7 @@ export function GridSection() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10 mx-auto w-full">
       <WobbleCard
-        containerClassName="col-span-1 lg:col-span-2 h-full bg-black min-h-[600px] lg:min-h-[300px] relative overflow-hidden rounded-2xl"
+        containerClassName="col-span-1 lg:col-span-2 h-full bg-black min-h-[600px] lg:min-h-[300px] relative overflow-hidden rounded-2xl group"
         className="z-10 relative"
       >
         <div className="max-w-xs relative z-30">
@@ -23,7 +23,7 @@ export function GridSection() {
             className="mt-4 inline-block bg-white text-black px-4 py-2 rounded-md"
             href="/products/athena"
           >
-            Ver productos 
+            Ver productos Athena
           </Link>
         </div>
         <Image
@@ -31,11 +31,11 @@ export function GridSection() {
           width={1000}
           height={1000}
           alt="linear demo image"
-          className="absolute inset-0 w-full h-full object-cover grayscale filter rounded-2xl opacity-40"
+          className="absolute inset-0 w-full h-full object-cover grayscale filter rounded-2xl opacity-40 group-hover:opacity-70 transition-opacity duration-300"
         />
       </WobbleCard>
       <WobbleCard
-        containerClassName="col-span-1 min-h-[300px] relative overflow-hidden"
+        containerClassName="col-span-1 min-h-[300px] bg-black relative overflow-hidden group"
         className="z-10 relative"
       >
         <Image
@@ -43,13 +43,15 @@ export function GridSection() {
           width={500}
           height={500}
           alt="linear demo image"
-          className="absolute inset-0 w-full h-full object-cover rounded-2xl opacity-40"
+          className="absolute inset-0 w-full h-full object-cover rounded-2xl opacity-40 group-hover:opacity-70 transition-opacity duration-300"
         />
-        <div className=" z-30 relative p-4">
-          <h2 className="text-right md:text-2xl lg:text-4xl font-bold tracking-[-0.015em] text-white">
-            EBC Frenos y embragues
+        <div className="z-30 relative p-4 flex flex-col items-end">
+          <h2 className="text-right md:text-2xl lg:text-6xl font-bold tracking-[-0.015em] text-white">
+            EBC
           </h2>
-
+          <p className="mt-4 text-right text-xl text-neutral-100 font-semibold">
+            Frenos y embragues
+          </p>
           <Link
             className="mt-4 inline-block bg-white text-black px-4 py-2 rounded-md"
             href="/products/ebc"
@@ -57,14 +59,6 @@ export function GridSection() {
             Ver productos EBC
           </Link>
         </div>
-      </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
-        <h2 className="text-left md:text-2xl lg:text-6xl font-bold tracking-[-0.015em] text-white">
-          No shirt, no shoes, no weapons.
-        </h2>
-        <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          If someone yells "stop!", goes limp, or taps out, the fight is over.
-        </p>
       </WobbleCard>
     </div>
   );
